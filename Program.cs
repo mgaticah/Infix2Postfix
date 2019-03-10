fc9852f35b976b54fb6fd1c17c6d6b6b1985952f
magh <manuel.gatica.holtmann@gmail.com>
2019-03-10 19:48:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infix2Postfix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
